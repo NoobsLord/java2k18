8f1a430e95b25169f689280f9953eb2f74fdb2bf
root <root@debian>
2018-05-01 17:36:03 +0200

    Int user

---
+++ src/ui/showImage.java
@@ -0,0 +1,31 @@
+package ui;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class showImage extends JPanel {
+	
+	BufferedImage  image;
+	
+	public showImage(String chemin_de_limage) {
+		    try {
+		 
+		  File input = new File(chemin_de_limage);
+		      image = ImageIO.read(input);
+		    } catch (IOException ie) {
+		      System.out.println("Error:"+ie.getMessage());
+		    }
+		  }
+		 
+		// cette méthode c'est elle qui va afficher l'image
+		  public void paint(Graphics g) {
+		    g.drawImage( image, 0, 0, null);
+		  }
+		 
+
+}
