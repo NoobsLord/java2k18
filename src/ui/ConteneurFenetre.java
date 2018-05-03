905443c141830a31fa18e5748559d5330ab0c3be
root <root@debian>
2018-05-04 01:18:51 +0200

    user int

--- src/ui/ConteneurFenetre.java
+++ src/ui/ConteneurFenetre.java
@@ -7,12 +7,9 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -20,13 +17,14 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import modele.*;
 
 
 public class ConteneurFenetre extends JPanel implements ActionListener {
-	public int indice=0;
+	public static int indice=0;
 
 	private JButton album;
 	private JButton favoris;
@@ -41,6 +39,10 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 	private JButton valider;
 	private JButton suivant;
 	private JButton precedent;
+	private JButton addToAlbum;
+	
+	private JRadioButton couleur;
+	private JRadioButton nom;
 
 	private JComboBox combo1;
 	private JComboBox combo2;
@@ -53,18 +55,13 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 	private JLabel mocle3;
 	private JLabel mocle4; 
 	private JLabel mocle5; 
+	private JLabel listAlbum;
 
 	private JTextField champText;
-
-	public static ArrayList<Images> active = new ArrayList<Images>();
-
+	private ArrayList<JPanel> wesh = new ArrayList<JPanel>();
+	
 	private int center_width;
 	private int center_height;
-
-	private BufferedImage fav_on;
-	private BufferedImage fav_off;
-
-
 
 
 
@@ -73,41 +70,38 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 		super();
 		this.proprieteConteneur();
 
+
 	}
 
 	public void paintComponent(Graphics g){
 		// Couleur de fond fenetre
-
-		try {
-			fav_on = ImageIO.read(new File("./star_on.png"));
-			fav_off = ImageIO.read(new File("./star_off.png"));
-		} catch (IOException e) {
-		}
-
 		super.paintComponent(g);
 		this.setBackground(new Color(200,200,200));
 		int x1=this.getWidth()/4;
 		int y1=this.getHeight()/4;
 		int y2=this.getHeight()-130;
-		//System.out.println("repaint");
+		System.out.println("repaint");
 		g.drawLine(0,80,this.getWidth(),80);
 		g.drawLine(0, 130, this.getWidth(), 130);
 		g.drawLine(this.getWidth()-890, this.getHeight()-y2, this.getWidth()-890, this.getHeight());
 		g.drawLine(this.getWidth()-200, 130, this.getWidth()-200, this.getHeight());
 		this.setVisible(true);
 
-		if (active.size() > 0) {
+		/*for (int i =0;i<wesh.size();i++) {
+			g.drawImage(Modele.all.get(i).img, wesh.get(i).getWidth(), wesh.get(i).getHeight(), this);
+		}*/
+		if (Modele.all.size() > 0) {
 			JPanel pan = new JPanel();
-
+			
 			center_width = (this.getWidth()/2)+(310/2);
 			center_height = (this.getHeight()/2)+(130/2)-50;
 			this.boutonSuivant();
 			this.boutonPrecedent();
-			Images ancour = active.get(indice);
-			Dimension vanilla = new Dimension(ancour.dim_x,ancour.dim_y);
+			Images ancour = Modele.all.get(indice);
+			Dimension vanilla = new Dimension(Modele.all.get(indice).dim_x,Modele.all.get(indice).dim_y);
 			Dimension dim = Controleur.dimension_ratio(vanilla);
-
-			Image current = ancour.img.getScaledInstance(dim.width, dim.height, Image.SCALE_SMOOTH);
+			
+			Image current = Modele.all.get(indice).img.getScaledInstance(dim.width, dim.height, Image.SCALE_SMOOTH);
 			g.drawImage(current, center_width-(dim.width/2)-100,center_height-(dim.height/2),this);
 			g.drawString("dimensions : "+ancour.dim_x+" x "+ancour.dim_y, this.getWidth()-190, 180);
 			g.drawString("couleur moyenne : ", this.getWidth()-190, 230);
@@ -120,16 +114,28 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 			g.fillRect(this.getWidth()-75, 270, 50, 50);
 			g.setColor(Color.black);
 			g.drawRect(this.getWidth()-75, 270, 50, 50);
-			g.drawString((indice+1) + " / " + active.size(), 666, 750);
-
-			if(ancour.favori) {
-				g.drawImage(fav_on, this.getWidth()-75, 10, this);
-			}
-			else {
-				g.drawImage(fav_off, this.getWidth()-75, 10, this);
-			}
-			//System.out.println(indice);
 		}
+	}
+
+	public void affichement() {
+		/*wesh = new ArrayList<JPanel>();
+		for (int i=0;i<Modele.all.size();i++) {
+			System.out.println(Modele.all.get(i).path);
+			JPanel test = new JPanel();
+			test.setPreferredSize(new Dimension(150,150));
+			if(i<=5) {
+				test.setBounds((150 + 28)*i,130, 150, 150);
+			}
+			else if (i<=10) {
+				test.setBounds((150 + 28)*i,300, 150, 150);
+			}
+
+			this.add(test);
+			wesh.add(test);
+		}
+		 */
+		indice = 0;
+		repaint();
 	}
 
 	private void proprieteConteneur(){
@@ -148,62 +154,32 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 		this.proprieteMocle();
 		this.proprieteComboBox();
 		this.proprieteChampText();
+		this.bouttonAjourterAlbum();
 
 	}
-
+	
 	private void boutonSuivant() {
 		suivant = new JButton();
 		this.suivant.setBounds(center_width-70,this.getHeight()-70,50,50);
 		this.suivant.setText(">>");
-		suivant.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (indice+1==active.size()-1) {
-					suivant.setVisible(false);
-				}
-				if (indice < active.size()-1) {
-					precedent.setVisible(true);
-					indice +=1;
-				}
-				repaint();
-			}
-		});
 		this.add(suivant);
-		//this.suivant.addActionListener(this);
+		this.suivant.addActionListener(next());
 	}
-
+	
 	private void boutonPrecedent() {
 		precedent = new JButton();
 		this.precedent.setBounds(center_width-130,this.getHeight()-70,50,50);
 		this.precedent.setText("<<");
-		precedent.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (indice-1 == 0) {
-					precedent.setVisible(false);
-				}
-				if (indice > 0) {
-					suivant.setVisible(true);
-					indice -=1;
-				}
-
-				
-				repaint();
-			}
-		});
 		this.add(precedent);
-		//this.precedent.addActionListener(this);
+		this.precedent.addActionListener(prev());
 	}
 
 	private void boutonAlbum(){
 		album= new JButton();
 		this.album.setBounds(420, 10, 100, 50);
 		this.album.setText("Album");
-		album.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				repaint();
-			}
-		});
 		this.add(album);
-		//this.album.addActionListener(this);
+		this.album.addActionListener(this);
 
 	}
 
@@ -211,12 +187,6 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 		favoris= new JButton();
 		this.favoris.setBounds(540, 10, 100, 50);
 		this.favoris.setText("Favoris");
-		favoris.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Controleur.toggle_favoris(active.get(indice));
-				repaint();
-			}
-		});
 		this.add(favoris);
 	}
 
@@ -225,8 +195,21 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 		this.ajouter.setBounds(660,10,100,50);
 		this.ajouter.setText("Ajouter");
 		this.add(ajouter);
-		//this.ajouter.addActionListener(this);
+		this.ajouter.addActionListener(this);
 
+	}
+	
+	private void bouttonAjourterAlbum(){
+		addToAlbum= new JButton();
+		this.addToAlbum.setBounds(1035, 400, 130, 30);
+		this.addToAlbum.setText("Add to Album");
+		this.addToAlbum.setForeground(Color.BLACK);
+		this.addToAlbum.setBackground(Color.CYAN);
+		this.addToAlbum.setBorderPainted(true);
+		this.addToAlbum.setFocusPainted(true);
+		Font f= new Font("Arial", Font.ITALIC, 12);
+		this.addToAlbum.setFont(f);
+		this.addToAlbum.addActionListener(this);
 	}
 
 	private void bouttonQuitter(){
@@ -241,7 +224,7 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 		quitter.setFont(f);
 		this.add(quitter);
 		//Evenement
-		//this.quitter.addActionListener(this);
+		this.quitter.addActionListener(this);
 	}
 
 	private void bouttonSupprimer(){
@@ -303,7 +286,7 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 		this.rechercher.setBounds(30, 150, 120, 30);
 		this.rechercher.setText("Rechercher");
 		this.add(rechercher);
-		//this.rechercher.addActionListener(this);
+		this.rechercher.addActionListener(this);
 	}
 
 	private void bouttonTrier(){
@@ -311,7 +294,7 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 		this.trier.setBounds(160, 150, 120, 30);
 		this.trier.setText("Tier");
 		this.add(trier);
-		//this.trier.addActionListener(this);
+		this.trier.addActionListener(this);
 	}
 
 	private void bouttonValider(){
@@ -347,192 +330,231 @@ public class ConteneurFenetre extends JPanel implements ActionListener {
 		mocle4= new JLabel();
 		mocle5= new JLabel();
 
-		this.mocle1.setBounds(50, 220, 120, 25);
+		this.mocle1.setBounds(50, 240, 120, 25);
 		this.add(mocle1);
 
-		this.mocle2.setBounds(50, 270, 120, 25);
+		this.mocle2.setBounds(50, 290, 120, 25);
 		this.add(mocle2);
 
-		this.mocle3.setBounds(50, 320, 120, 25);
+		this.mocle3.setBounds(50, 340, 120, 25);
 		this.add(mocle3);
 
-		this.mocle4.setBounds(50, 370, 120, 25);
+		this.mocle4.setBounds(50, 390, 120, 25);
 		this.add(mocle4);
 
-		this.mocle5.setBounds(50, 420, 120, 25);
+		this.mocle5.setBounds(50, 440, 120, 25);
 		this.add(mocle5);
 
 	}
-
-	/*public ActionListener next() {
+	
+	public static ActionListener next() {
 		indice += 1;
-		repaint();
 		return null;
 	}
-
-	public ActionListener prev() {
+	
+	public static ActionListener prev() {
 		indice -= 1;
-		repaint();
 		return null;
 	}
 
+	
+	
+	
+	
+	
+	
+	//Fonctions Evenement boutton
+	
+	
+	public void eventAlbum(){
+		this.mocle1.setVisible(false);
+		this.mocle2.setVisible(false);
+		this.mocle3.setVisible(false);
+		this.mocle4.setVisible(false);
+		this.mocle5.setVisible(false);
 
-	public ActionListener ajout_manuel() {
+		this.combo1.setVisible(false);
+		this.combo2.setVisible(false);
+		this.combo3.setVisible(false);
+		this.combo4.setVisible(false);
+		this.combo5.setVisible(false);
+
+		this.champText.setVisible(false);
+
+		this.valider.setVisible(false);
+
+	}
+	
+	public void eventTrier(){
+		couleur= new JRadioButton();
+		this.couleur.setText("Par couleur");
+		this.couleur.setBounds(50, 200, 120, 25);
+		this.add(couleur);
+		
+		nom= new JRadioButton();
+		this.nom.setText("Par nom");
+		this.nom.setBounds(180, 200, 120, 25);
+		this.add(nom);
+		
+		this.mocle1.setText("Mot Clé 1");
+		this.mocle2.setText("Mot Clé 2");
+		this.mocle3.setText("Mot Clé 3");
+		this.mocle4.setText("Mot Clé 4");
+		this.mocle5.setText("Mot Clé 5");
+
+		this.mocle1.setVisible(true);
+		this.mocle2.setVisible(true);
+		this.mocle3.setVisible(true);
+		this.mocle4.setVisible(true);
+		this.mocle5.setVisible(true);
+
+		combo1.setBounds(140, 240, 120, 25);
+		combo1.addItem("Option 1");
+		combo1.addItem("Option 2");
+		combo1.addItem("Option 3");
+		combo1.addItem("Option 4");
+		this.combo1.setVisible(true);
+
+
+		combo2.setBounds(140, 290, 120, 25);
+		combo2.addItem("Option 1");
+		combo2.addItem("Option 2");
+		combo2.addItem("Option 3");
+		combo2.addItem("Option 4");
+		this.combo2.setVisible(true);
+
+		combo3.setBounds(140, 340, 120, 25);
+		combo3.addItem("Option 1");
+		combo3.addItem("Option 2");
+		combo3.addItem("Option 3");
+		combo3.addItem("Option 4");
+		this.combo3.setVisible(true);
+
+		combo4.setBounds(140, 390, 120, 25);
+		combo4.addItem("Option 1");
+		combo4.addItem("Option 2");
+		combo4.addItem("Option 3");
+		combo4.addItem("Option 4");
+		this.combo4.setVisible(true);
+
+		combo5.setBounds(140, 440, 120, 25);
+		combo5.addItem("Option 1");
+		combo5.addItem("Option 2");
+		combo5.addItem("Option 3");
+		combo5.addItem("Option 4");
+		this.combo5.setVisible(true);
+
+		this.valider.setBounds(105, 510, 80, 30);
+		this.valider.setText("Valider");
+		this.valider.setForeground(Color.BLACK);
+		this.valider.setBackground(Color.WHITE);
+		this.valider.setBorderPainted(true);
+		this.valider.setFocusPainted(true);
+		Font f=new Font("Arial", Font.ITALIC, 11);
+		this.valider.setFont(f);
+		this.valider.setVisible(true);
+
+		this.champText.setVisible(false);
+
+
+	}
+	
+	public void eventRechercher(){
+		this.champText.setBounds(50, 220, 190, 30);
+		this.champText.setBackground(Color.WHITE);
+		this.champText.setVisible(true);
+
+		this.valider.setBounds(105, 260, 80, 30);
+		this.valider.setText("Valider");
+		this.valider.setForeground(Color.BLACK);
+		this.valider.setBackground(Color.WHITE);
+		this.valider.setBorderPainted(true);
+		this.valider.setFocusPainted(true);
+		Font f=new Font("Arial", Font.ITALIC, 11);
+		this.valider.setFont(f);
+		this.valider.setVisible(true);
+
+
+		this.mocle1.setVisible(false);
+		this.mocle2.setVisible(false);
+		this.mocle3.setVisible(false);
+		this.mocle4.setVisible(false);
+		this.mocle5.setVisible(false);
+
+
+		this.combo1.setVisible(false);
+		this.combo2.setVisible(false);
+		this.combo3.setVisible(false);
+		this.combo4.setVisible(false);
+		this.combo5.setVisible(false);
+
+		Controleur.tri_couleur(Color.red);
+	}
+	
+	public void eventAjouter(){
 		JFileChooser fic = new JFileChooser();
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF & PNG Images", "jpg", "gif","png");
 		fic.setFileFilter(filter);
 		if (fic.showOpenDialog(this) == JFileChooser.APPROVE_OPTION)
-			try {
+			//try {
 				Controleur.ajouter_image(fic.getSelectedFile().getPath());
-			} catch (ExceptionNomUtilise e1) {
+				this.add(addToAlbum);
+			/*} catch (ExceptionNomUtilise e1) {
 				e1.printStackTrace();
-			}
-		repaint();
-		return null;
-	}*/
+			}*/
+	}
+	
+	public void eventAddToAlbum(){
+		
+		listAlbum= new JLabel();
+		this.listAlbum.setText("Choisir");
+		this.listAlbum.setBounds(1065, 450, 120, 25);
+		this.listAlbum.setVisible(true);
+		this.add(listAlbum);
+		
+		combo1.setBounds(1040, 480, 120, 25);
+		combo1.addItem("Album 1");
+		combo1.addItem("Album 2");
+		combo1.addItem("Album 3");
+		combo1.addItem("Album 4");
+		this.combo1.setVisible(true);
+		
+	}
 
 
 
+	//Condition d'evenements
+	
 	public void actionPerformed(ActionEvent e) {
 
-		/*if(e.getSource()== this.album){
-			this.mocle1.setVisible(false);
-			this.mocle2.setVisible(false);
-			this.mocle3.setVisible(false);
-			this.mocle4.setVisible(false);
-			this.mocle5.setVisible(false);
+		if(e.getSource()== this.album){
+			this.eventAlbum();
 
-			this.combo1.setVisible(false);
-			this.combo2.setVisible(false);
-			this.combo3.setVisible(false);
-			this.combo4.setVisible(false);
-			this.combo5.setVisible(false);
+			//#######################
 
-			this.champText.setVisible(false);
+			//affichement();
 
-			this.valider.setVisible(false);
-
+			//#######################
 		}
 		else if(e.getSource()== this.trier){
-			this.mocle1.setText("Mot Clé 1");
-			this.mocle2.setText("Mot Clé 2");
-			this.mocle3.setText("Mot Clé 3");
-			this.mocle4.setText("Mot Clé 4");
-			this.mocle5.setText("Mot Clé 5");
-
-			this.mocle1.setVisible(true);
-			this.mocle2.setVisible(true);
-			this.mocle3.setVisible(true);
-			this.mocle4.setVisible(true);
-			this.mocle5.setVisible(true);
-
-			combo1.setBounds(140, 220, 120, 25);
-			combo1.addItem("Option 1");
-			combo1.addItem("Option 2");
-			combo1.addItem("Option 3");
-			combo1.addItem("Option 4");
-			this.combo1.setVisible(true);
-
-
-			combo2.setBounds(140, 270, 120, 25);
-			combo2.addItem("Option 1");
-			combo2.addItem("Option 2");
-			combo2.addItem("Option 3");
-			combo2.addItem("Option 4");
-			this.combo2.setVisible(true);
-
-			combo3.setBounds(140, 320, 120, 25);
-			combo3.addItem("Option 1");
-			combo3.addItem("Option 2");
-			combo3.addItem("Option 3");
-			combo3.addItem("Option 4");
-			this.combo3.setVisible(true);
-
-			combo4.setBounds(140, 370, 120, 25);
-			combo4.addItem("Option 1");
-			combo4.addItem("Option 2");
-			combo4.addItem("Option 3");
-			combo4.addItem("Option 4");
-			this.combo4.setVisible(true);
-
-			combo5.setBounds(140, 420, 120, 25);
-			combo5.addItem("Option 1");
-			combo5.addItem("Option 2");
-			combo5.addItem("Option 3");
-			combo5.addItem("Option 4");
-			this.combo5.setVisible(true);
-
-			this.valider.setBounds(105, 490, 80, 30);
-			this.valider.setText("Valider");
-			this.valider.setForeground(Color.BLACK);
-			this.valider.setBackground(Color.WHITE);
-			this.valider.setBorderPainted(true);
-			this.valider.setFocusPainted(true);
-			Font f=new Font("Arial", Font.ITALIC, 11);
-			this.valider.setFont(f);
-			this.valider.setVisible(true);
-
-			this.champText.setVisible(false);
+			this.eventTrier();
 
 
 		}
 		else if(e.getSource()== this.rechercher){
-			this.champText.setBounds(50, 220, 190, 30);
-			this.champText.setBackground(Color.WHITE);
-			this.champText.setVisible(true);
-
-			this.valider.setBounds(105, 260, 80, 30);
-			this.valider.setText("Valider");
-			this.valider.setForeground(Color.BLACK);
-			this.valider.setBackground(Color.WHITE);
-			this.valider.setBorderPainted(true);
-			this.valider.setFocusPainted(true);
-			Font f=new Font("Arial", Font.ITALIC, 11);
-			this.valider.setFont(f);
-			this.valider.setVisible(true);
-
-
-			this.mocle1.setVisible(false);
-			this.mocle2.setVisible(false);
-			this.mocle3.setVisible(false);
-			this.mocle4.setVisible(false);
-			this.mocle5.setVisible(false);
-
-
-			this.combo1.setVisible(false);
-			this.combo2.setVisible(false);
-			this.combo3.setVisible(false);
-			this.combo4.setVisible(false);
-			this.combo5.setVisible(false);
-
-			Controleur.tri_couleur(Color.red);
-
+			this.eventRechercher();
 		}
 		else if(e.getSource()== this.ajouter){
-			JFileChooser fic = new JFileChooser();
-			FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF & PNG Images", "jpg", "gif","png");
-			fic.setFileFilter(filter);
-			if (fic.showOpenDialog(this) == JFileChooser.APPROVE_OPTION)
-				try {
-					Controleur.ajouter_image(fic.getSelectedFile().getPath());
-				} catch (ExceptionNomUtilise e1) {
-					e1.printStackTrace();
-				}
+			this.eventAjouter();
 		}
-		else if(e.getSource()== this.suivant){
-			if (indice < Modele.all.size()) {
-				indice +=1;
-			}
-
+		else if(e.getSource()== this.addToAlbum){
+			this.eventAddToAlbum();
 		}
-		else if(e.getSource()== this.precedent){
-			if (indice > 0) {
-				indice -= 1;
-			}
+		repaint();
 
-		}*/
-		//repaint();
 
-	}		
+	}
+
+
 }
 
